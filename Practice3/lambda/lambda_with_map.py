a7abd82139ea80006ed0ca764b6705a98f9558a0
selika06 <aselmed33@gmail.com>
2026-06-17 12:24:56 +0500

    add practice2

---
+++ Practice3/lambda/lambda_with_map.py
@@ -0,0 +1,11 @@
+# Lambda with Built-in Functions: Lambda functions are commonly used with built-in functions like map(), filter(), and sorted().
+
+# Using Lambda with map()
+# The map() function applies a function to every item in an iterable:
+
+# Example
+# Double all numbers in a list:
+
+numbers = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x: x * 2, numbers))
+print(doubled)
