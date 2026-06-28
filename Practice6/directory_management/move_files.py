10f4c383aa64a9fcb1fda1ca447a114a711e39e3
selika06 <aselmed33@gmail.com>
2026-06-28 23:18:25 +0500

    add practice6

--- Practice6/directory_management/move_files.py
+++ Practice6/directory_management/move_files.py
@@ -1,11 +1,15 @@
-import shutil 
-import os 
-os.makedirs("Backup", exist_ok=True) 
-# Copy file 
-shutil.copy("sample.txt", "Backup/sample.txt") 
-# Move file 
-shutil.move("Backup/sample.txt", "sample_moved.txt") 
-# Find txt files 
-for file in os.listdir(): 
-    if file.endswith(".txt"): 
+import shutil
+import os
+
+os.makedirs("Backup", exist_ok=True)
+
+# Copy file from file_handling folder
+shutil.copy("../file_handling/sample.txt", "Backup/sample.txt")
+
+# Move file
+shutil.move("Backup/sample.txt", "sample_moved.txt")
+
+# Find txt files
+for file in os.listdir():
+    if file.endswith(".txt"):
         print(file)
