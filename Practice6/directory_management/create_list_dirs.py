65263c6ce1e9a1d678e5297cc43f0a5c95ab3b9b
selika06 <aselmed33@gmail.com>
2026-06-28 23:24:04 +0500

    add practice6

--- Practice6/directory_management/create_list_dirs.py
+++ Practice6/directory_management/create_list_dirs.py
@@ -1,11 +1,20 @@
-import os 
+import os
+
 # Create directory
 os.mkdir("TestFolder")
+
 # Create nested directories
 os.makedirs("FolderA/FolderB", exist_ok=True)
+
+# Create a test file inside FolderB
+with open("FolderA/FolderB/test.txt", "w") as file:
+    file.write("This is a test file.")
+
 # Current working directory
 print(os.getcwd())
+
 # List files and folders
 print(os.listdir())
-# Remove directory
+
+# Remove TestFolder
 os.rmdir("TestFolder")
