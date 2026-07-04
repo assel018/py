9c40e09fef9bfc54d15e21969ad23545758d7740
selika06 <aselmed33@gmail.com>
2026-07-04 11:16:40 +0500

    add practice7

--- Practice7/config.py
+++ Practice7/config.py
@@ -1,20 +1,19 @@
-from configparser import ConfigParser
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "dbname": "phonebook",
+    "user": "asel"
+}
 
-def load_config(filename='database.ini', section='postgresql'):
-    parser = ConfigParser()
-    parser.read(filename)
-
-    # get section, default to postgresql
-    config = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            config[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-    return config
-
-if __name__ == '__main__':
-    config = load_config()
-    print(config)
+MENU = """
+=========================
+        PHONEBOOK
+=========================
+1. Show all contacts
+2. Insert contact
+3. Import from CSV
+4. Update contact
+5. Search contact
+6. Delete contact
+7. Exit
+"""
