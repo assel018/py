3955acd34c48ee142aeecddafe148c63102e7ac3
selika06 <aselmed33@gmail.com>
2026-07-09 13:31:55 +0500

    Add tsis - PhoneBook

--- tsis/schema.sql
+++ tsis/schema.sql
@@ -4,13 +4,13 @@ DROP TABLE IF EXISTS groups CASCADE;
 
 CREATE TABLE groups (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL
+    name VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE phonebook (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE,
-    email VARCHAR(100),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE,
     birthday DATE,
     group_id INTEGER REFERENCES groups(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -20,7 +20,7 @@ CREATE TABLE phones (
     id SERIAL PRIMARY KEY,
     contact_id INTEGER REFERENCES phonebook(id)
         ON DELETE CASCADE,
-    phone VARCHAR(20) NOT NULL,
+    phone VARCHAR(20) UNIQUE NOT NULL,
     type VARCHAR(10)
         CHECK(type IN ('home','work','mobile'))
 );
